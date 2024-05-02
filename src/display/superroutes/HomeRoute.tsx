d7d5aa773fbd6083f5813c3ef609767f1ff217b2
Bob-The-Seagull-King <laneedwardsbrown@gmail.com>
2024-05-03 10:51:16 +1200

    bio pages open modals

--- src/display/superroutes/HomeRoute.tsx
+++ src/display/superroutes/HomeRoute.tsx
@@ -1,18 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import '../../resources/styles/_icon.scss'
 import React from 'react'
+import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { ROUTES } from '../../resources/routes-constants'
 import { useNavigate } from "react-router-dom";
 
-import { AllAbilitiesListPage } from '../../classes/viewmodel/pages/AllAbilitiesListPage'
+import BioInformation from '../components/subcomponents/informationpanel/BioInformation'
+import SocialInformation from '../components/subcomponents/informationpanel/SocialsInformation'
 
 import logo from '../../resources/images/KOS-Logo.png'
-import ReactDOM from 'react-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFistRaised } from '@fortawesome/free-solid-svg-icons'
-import { faHeart } from '@fortawesome/free-solid-svg-icons'
-import { faLink } from '@fortawesome/free-solid-svg-icons'
 import { faWrench } from '@fortawesome/free-solid-svg-icons'
 import { faBook } from '@fortawesome/free-solid-svg-icons'
 import { faPaintBrush } from '@fortawesome/free-solid-svg-icons'
@@ -21,9 +19,6 @@ import { faFilePen } from '@fortawesome/free-solid-svg-icons'
 
 const HomeRoute: React.FC = () => {
     const navigate = useNavigate();
-
-    // Initialize Controller //
-    const AbilitiesCollectionController = new AllAbilitiesListPage()    
 
     function NavigateHome(dir: string) {
         navigate('/' + dir);
@@ -42,21 +37,11 @@ const HomeRoute: React.FC = () => {
                     </div>
                     <div className="row row-cols-lg-2 row-cols-md-2 row-cols-sx-1 row-cols-xs-1 row-cols-1">
                         <div className="col">
-                            <div className="pageaccessbox bordersubpurple hovermouse" onClick={() => NavigateHome("compendium/abilities/")}>
-                                <FontAwesomeIcon icon={faHeart} className="pageaccestext"/>
-                                <h1 className="pageaccestext">
-                                    INTRODUCTION
-                                </h1>
-                            </div>
+                            <BioInformation/>
                             <div className="verticalspacermed"/>
                         </div>
                         <div className="col">
-                            <div className="pageaccessbox bordersubpurple hovermouse" onClick={() => NavigateHome("compendium/abilities/")}>
-                                <FontAwesomeIcon icon={faLink} className="pageaccestext"/>
-                                <h1 className="pageaccestext">
-                                    SOCIALS
-                                </h1>
-                            </div>
+                            <SocialInformation/>
                             <div className="verticalspacermed"/>
                         </div>
                     </div>
