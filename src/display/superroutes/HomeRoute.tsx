c19b4a66aa026458f2c16e5ed182d463c7779ae4
Bob-The-Seagull-King <laneedwardsbrown@gmail.com>
2024-05-03 09:42:16 +1200

    homepage + logo + colour

--- src/display/superroutes/HomeRoute.tsx
+++ src/display/superroutes/HomeRoute.tsx
@@ -7,11 +7,17 @@ import { useNavigate } from "react-router-dom";
 
 import { AllAbilitiesListPage } from '../../classes/viewmodel/pages/AllAbilitiesListPage'
 
-import logo from '../../resources/images/iconpendium_logo.png'
+import logo from '../../resources/images/KOS-Logo.png'
 import ReactDOM from 'react-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFistRaised } from '@fortawesome/free-solid-svg-icons'
-import { faFileLines } from '@fortawesome/free-solid-svg-icons'
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
+import { faLink } from '@fortawesome/free-solid-svg-icons'
+import { faWrench } from '@fortawesome/free-solid-svg-icons'
+import { faBook } from '@fortawesome/free-solid-svg-icons'
+import { faPaintBrush } from '@fortawesome/free-solid-svg-icons'
+import { faPen } from '@fortawesome/free-solid-svg-icons'
+import { faFilePen } from '@fortawesome/free-solid-svg-icons'
 
 const HomeRoute: React.FC = () => {
     const navigate = useNavigate();
@@ -30,35 +36,83 @@ const HomeRoute: React.FC = () => {
                     <div className="row">
                         <img src={logo} style={{maxWidth:"100%"}} />
                     </div>
-                    <div className='row'><div className='col'><br/></div></div>
+                    <div className='row'><div className='col'></div></div>
                     <div className="row">
-                        <div className="separator"><h3 style={{fontFamily:"Libre Baskerville"}}>Compendium</h3></div>
+                        <div className="separator"><h3 style={{fontFamily:"Libre Baskerville"}}>Bio</h3></div>
                     </div>
-                    <div className="row">
-                        <div className="col row-cols-lg-1 row-cols-md-1 row-cols-sx-1 row-cols-xs-1 row-cols-1">
-                            <div className="col">
-                                <div className="pageaccessbox bordersubpurple hovermouse" onClick={() => NavigateHome("compendium/abilities/")}>
-                                    <FontAwesomeIcon icon={faFistRaised} className="pageaccestext"/>
-                                    <h1 className="pageaccestext">
-                                        ABILITIES
-                                    </h1>
-                                </div>
+                    <div className="row row-cols-lg-2 row-cols-md-2 row-cols-sx-1 row-cols-xs-1 row-cols-1">
+                        <div className="col">
+                            <div className="pageaccessbox bordersubpurple hovermouse" onClick={() => NavigateHome("compendium/abilities/")}>
+                                <FontAwesomeIcon icon={faHeart} className="pageaccestext"/>
+                                <h1 className="pageaccestext">
+                                    INTRODUCTION
+                                </h1>
                             </div>
+                            <div className="verticalspacermed"/>
+                        </div>
+                        <div className="col">
+                            <div className="pageaccessbox bordersubpurple hovermouse" onClick={() => NavigateHome("compendium/abilities/")}>
+                                <FontAwesomeIcon icon={faLink} className="pageaccestext"/>
+                                <h1 className="pageaccestext">
+                                    SOCIALS
+                                </h1>
+                            </div>
+                            <div className="verticalspacermed"/>
                         </div>
                     </div>
                     <div className="row">
-                        <div className="separator"><h3 style={{fontFamily:"Libre Baskerville"}}>Tools</h3></div>
+                        <div className="separator"><h3 style={{fontFamily:"Libre Baskerville"}}>Projects</h3></div>
+                    </div>
+                    <div className="row row-cols-lg-2 row-cols-md-2 row-cols-sx-1 row-cols-xs-1 row-cols-1">
+                        <div className="col">
+                            <div className="pageaccessbox bordersubpurple hovermouse" onClick={() => NavigateHome("compendium/abilities/")}>
+                                <FontAwesomeIcon icon={faWrench} className="pageaccestext"/>
+                                <h1 className="pageaccestext">
+                                    ICONPENDIUM
+                                </h1>
+                            </div>
+                            <div className="verticalspacermed"/>
+                        </div>
+                        <div className='col'>
+                            <div className="pageaccessbox bordersubpurple hovermouse" onClick={() => NavigateHome("compendium/abilities/")}>
+                                <FontAwesomeIcon icon={faBook} className="pageaccestext"/>
+                                <h1 className="pageaccestext">
+                                    VISIONS UNVEILED
+                                </h1>
+                            </div>
+                            <div className="verticalspacermed"/>
+                        </div>
                     </div>
                     <div className="row">
-                        <div className="col row-cols-lg-1 row-cols-md-1 row-cols-sx-1 row-cols-xs-1 row-cols-1">
-                            <div className="col">
-                                <div className="pageaccessbox bordersubpurple hovermouse" onClick={() => NavigateHome("tools/content/")}>
-                                    <FontAwesomeIcon icon={faFileLines} className="pageaccestext"/>
-                                    <h1 className="pageaccestext">
-                                        CONTENT MANAGER
-                                    </h1>
-                                </div>
+                        <div className="separator"><h3 style={{fontFamily:"Libre Baskerville"}}>Catalogs</h3></div>
+                    </div>
+                    <div className="row row-cols-lg-2 row-cols-md-2 row-cols-sx-1 row-cols-xs-1 row-cols-1">
+                        <div className="col">
+                            <div className="pageaccessbox bordersubpurple hovermouse" onClick={() => NavigateHome("compendium/abilities/")}>
+                                <FontAwesomeIcon icon={faPaintBrush} className="pageaccestext"/>
+                                <h1 className="pageaccestext">
+                                    ART
+                                </h1>
                             </div>
+                            <div className="verticalspacermed"/>
+                        </div>
+                        <div className="col">
+                            <div className="pageaccessbox bordersubpurple hovermouse" onClick={() => NavigateHome("compendium/abilities/")}>
+                                <FontAwesomeIcon icon={faPen} className="pageaccestext"/>
+                                <h1 className="pageaccestext">
+                                    WRITINGS
+                                </h1>
+                            </div>
+                            <div className="verticalspacermed"/>
+                        </div>
+                        <div className="col">
+                            <div className="pageaccessbox bordersubpurple hovermouse" onClick={() => NavigateHome("compendium/abilities/")}>
+                                <FontAwesomeIcon icon={faFilePen} className="pageaccestext"/>
+                                <h1 className="pageaccestext">
+                                    BLOG
+                                </h1>
+                            </div>
+                            <div className="verticalspacermed"/>
                         </div>
                     </div>
                     <div className="row">
